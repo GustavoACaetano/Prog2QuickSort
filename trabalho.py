efbc2d8d59c14c82ced39c256df0ec7a7b5dc384
GustavoACaetano <gustavo.gustavovat@gmail.com>
2025-01-21 08:50:51 -0300

    b

--- trabalho.py
+++ trabalho.py
@@ -1,20 +1,38 @@
+"""
+Feito por:
+
+- Gustavo Alves Caetano
+- João Pedro Zamborlini Barcellos
+"""
+
 import pickle
 import os
 
 
-def menu():
+def menu() -> None:
     print('---------------------------------- O melhor programa de ordenação! ----------------------------------')
     print('')
 
 
-def leArquivoBin(nomeArquivo):
-    if (os.path.isfile(f"{nomeArquivo}.bin")):
-        with open(f"{nomeArquivo}.bin", 'rb') as arq:
-            jogadores = pickle.load(arq)
+def read_file_bin(nome_arquivo: str):
+    if (file_exists(nome_arquivo)):
+        with open(f"{nome_arquivo}.bin", 'rb') as arquivo:
+            alunos = pickle.load(arquivo)
+    else:
+        read_file_bin(read_file_name())
+    return alunos
+
+
+def file_exists(nome_arquivo: str):
+    return os.path.isfile(f"{nome_arquivo}.bin")
+
+
+def read_file_name():
+    return input('Insira um nome de arquivo .bin existente nesta pasta: ')
 
 
 def main():
-    
+    print(read_file_bin(read_file_name()))
 
     return 0
 
